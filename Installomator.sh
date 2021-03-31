89caee5e670a4a61cfc623d85ab4a71cbdf8066d
Mort <37114226+bmortens115@users.noreply.github.com>
2021-03-31 16:07:00 -0700

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -20,7 +20,7 @@ export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 # set to 0 for production, 1 for debugging
 # while debugging, items will be downloaded to the parent directory of this script
 # also no actual installation will be performed
-DEBUG=1
+DEBUG=0
 
 # notify behavior
 NOTIFY=success
